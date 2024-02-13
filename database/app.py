b9e7a3f546b8945e50c3510fd9c2c37e66ebe5b1
Logan B <lb3attd@gmail.com>
2024-02-13 10:01:05 -0500

    for movies-prestage.html

--- database/app.py
+++ database/app.py
@@ -3,6 +3,9 @@ from flask import render_template, request, jsonify
 import config
 from models import Person
 
+import sqlite3
+import pandas as pd
+
 app = config.connex_app
 app.add_api(config.basedir / "swagger.yml")
 
@@ -12,6 +15,22 @@ def home():
     people = Person.query.all()
     return render_template("home.html", people=people)
 
+@app.route("/import_csv")
+def import_csv():
+    #Connect to SQLite DB
+    conn = sqlite3.connect()
+    
+    # Reads csv file into pandas dataframe
+    df = pd.read_csv('data/movies_prestage.db')
+    
+    # Write the dataframe to a SQLite table
+    df.to.sql('movies', conn, index=False, if_exists='replace')
+    
+    # Commit changes and close the connection
+    conn.commit()
+    conn.close()
+    
+    return "CSV data imported into the 'movies_prestage' database"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=False)
